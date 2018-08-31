244211463d073c7794311a05c4059cfd04f1361c
dhollenbeck1 <dhollenbeck@ucmerced.edu>
2018-08-30 17:41:39 -0700

    updated data

---
+++ xlsx2mat.m
@@ -0,0 +1,20 @@
+%% move data from xlsx to mat
+
+% 1. manually import data as column vectors
+% 2. update ang, coh, and cor 
+% 3. run script
+
+ang = 45;
+coh = 0;
+cor = 1;
+
+avg_t2e = VarName3;
+avg_t2d = VarName4;
+part = VarName5;
+tar = VarName6;
+
+fn = ['angle_',num2str(ang),'_coh_',num2str(coh),'_cor_',num2str(cor),'.mat'];
+save(fn,'avg_t2e','avg_t2d','part','tar')
+
+clc
+clear
